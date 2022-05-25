683a7d36c12795db2854160c3121eb878271eb6c
antoni.wrobel <antoni.wrobel@imaginecurve.com>
2022-05-25 21:28:33 +0200

    test

--- prisma/migrations/20220525172748_finished_res/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "FinishedReservation" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "reservationId" TEXT NOT NULL,
-    CONSTRAINT "FinishedReservation_reservationId_fkey" FOREIGN KEY ("reservationId") REFERENCES "Reservation" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
